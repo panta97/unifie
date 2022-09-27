9bf7d2f1ef6281668dfdb06a48605d8142db6141
Roosevelt <19panta97@gmail.com>
2022-09-26 21:55:56 -0500

    fix format

--- src/react/vite.config.ts
+++ src/react/vite.config.ts
@@ -1,14 +1,16 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+/** @type {import('vite').UserConfig} */
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 9000,
   },
   plugins: [react()],
-  base: '/react-static/',
+  base: "/react-static/",
   build: {
     sourcemap: true,
     manifest: true,
@@ -16,12 +18,12 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        'sales': 'web/k_sales/index.tsx',
-        'barcode': 'web/barcode/index.tsx',
-        'purchase_order_sheet': 'web/purchase_order_sheet/index.tsx',
-        'product_rpc': 'web/product_rpc/index.tsx',
-        'default': 'web/default/main.tsx'
+        sales: "web/k_sales/index.tsx",
+        barcode: "web/barcode/index.tsx",
+        purchase_order_sheet: "web/purchase_order_sheet/index.tsx",
+        product_rpc: "web/product_rpc/index.tsx",
+        default: "web/default/main.tsx",
       },
-    }
+    },
   },
-})
+});
