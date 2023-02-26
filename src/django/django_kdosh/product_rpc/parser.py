4facb3b4586504b5ce57c1ecc57d599c547c29f4
Roosevelt <19panta97@gmail.com>
2023-02-26 17:39:44 -0500

    fix purchase order link

--- src/django/django_kdosh/product_rpc/parser.py
+++ src/django/django_kdosh/product_rpc/parser.py
@@ -3,40 +3,53 @@ import datetime as dt
 from django.conf import settings
 from .utils.sql import select
 
+
 def transform_product_json(data):
     transf_obj = []
     for prod in data:
         default_code_map = []
         list_price_map = []
-        for attr_dc in prod['attr_default_code']:
-            default_code_map.append({
-                'ids': [attr_val_id['id'] for attr_val_id in attr_dc['attr_val_ids']],
-                'default_code': attr_dc['default_code'].strip(),
-            })
-        for attr_lp in prod['attr_list_price']:
-            list_price_map.append({
-                'ids': [attr_val_id['id'] for attr_val_id in attr_lp['attr_val_ids']],
-                'list_price': attr_lp['list_price']
-            })
+        for attr_dc in prod["attr_default_code"]:
+            default_code_map.append(
+                {
+                    "ids": [
+                        attr_val_id["id"] for attr_val_id in attr_dc["attr_val_ids"]
+                    ],
+                    "default_code": attr_dc["default_code"].strip(),
+                }
+            )
+        for attr_lp in prod["attr_list_price"]:
+            list_price_map.append(
+                {
+                    "ids": [
+                        attr_val_id["id"] for attr_val_id in attr_lp["attr_val_ids"]
+                    ],
+                    "list_price": attr_lp["list_price"],
+                }
+            )
         product = {
-            'name' : prod['name'].strip(),
-            'default_code' : prod['default_code'].strip(),
-            'list_price' : prod['list_price'],
-            'categ_id' : prod['category_brand_id'],
-            'pos_categ_id' : prod['pos_categ_id'],
-            'attribute_line_ids': [],
+            "name": prod["name"].strip(),
+            "default_code": prod["default_code"].strip(),
+            "list_price": prod["list_price"],
+            "categ_id": prod["category_brand_id"],
+            "pos_categ_id": prod["pos_categ_id"],
+            "attribute_line_ids": [],
         }
-        for attr in prod['attrs']:
-            product['attribute_line_ids'].append({
-                'attribute_id' : attr['attr']['id'],
-                'value_ids': [attr_val['id'] for attr_val in attr['attr_vals']]
-            })
-        transf_obj.append({
-            'product': product,
-            'default_code_map': default_code_map,
-            'list_price_map': list_price_map,
-            'client_id': prod['id']
-        })
+        for attr in prod["attrs"]:
+            product["attribute_line_ids"].append(
+                {
+                    "attribute_id": attr["attr"]["id"],
+                    "value_ids": [attr_val["id"] for attr_val in attr["attr_vals"]],
+                }
+            )
+        transf_obj.append(
+            {
+                "product": product,
+                "default_code_map": default_code_map,
+                "list_price_map": list_price_map,
+                "client_id": prod["id"],
+            }
+        )
 
     return transf_obj
 
@@ -46,7 +59,9 @@ def product_stats_get(product_tmpl_ids):
         select odoo_id, client_id
         from rpc_product_stats
         where odoo_id in ({});
-    """.format(",".join(map(str,product_tmpl_ids)))
+    """.format(
+        ",".join(map(str, product_tmpl_ids))
+    )
     res = select(sql)
     return res
 
@@ -56,14 +71,16 @@ def product_client_result(product_tmpl_ids):
     product_results = []
     for result in results:
         # RESULT TUPLE (odoo_id, client_id)
-        product_results.append({
-            "odoo_id": result[0],
-            "odoo_link": "{}/web#id={}&menu_id=193&cids=1&action=321&model=product.template&view_type=form"
-                .format(settings.ODOO_URL, result[0]),
-            "client_id": result[1],
-        })
+        product_results.append(
+            {
+                "odoo_id": result[0],
+                "odoo_link": "{}/web#id={}&menu_id=193&cids=1&action=321&model=product.template&view_type=form".format(
+                    settings.ODOO_URL, result[0]
+                ),
+                "client_id": result[1],
+            }
+        )
     return product_results
-
 
 
 def transform_order_json(data):
@@ -73,35 +90,41 @@ def transform_order_json(data):
     UNTAX_ID = 6
     PERUVIAN_TAX = 0.18
 
-    for order_item in data['order_list']:
-        for product_row in order_item['product_matrix']:
-            for product_item in product_row['product_items']:
-                if product_item['qty'] == 0 or product_item['price'] == 0:
+    for order_item in data["order_list"]:
+        for product_row in order_item["product_matrix"]:
+            for product_item in product_row["product_items"]:
+                if product_item["qty"] == 0 or product_item["price"] == 0:
                     continue
-                order_lines.append({
-                    "product_id": product_item['id'],
-                    "name": product_item['name'],
-                    "date_planned": order_item['date'],
-                    "product_qty": product_item['qty'],
-                    "price_unit": product_item['price'],
-                    "tax_id": TAX_ID if data['order_details']['is_taxed'] else UNTAX_ID
-                })
-                total_price += product_item['price'] * product_item['qty']
+                order_lines.append(
+                    {
+                        "product_id": product_item["id"],
+                        "name": product_item["name"],
+                        "date_planned": order_item["date"],
+                        "product_qty": product_item["qty"],
+                        "price_unit": product_item["price"],
+                        "tax_id": TAX_ID
+                        if data["order_details"]["is_taxed"]
+                        else UNTAX_ID,
+                    }
+                )
+                total_price += product_item["price"] * product_item["qty"]
 
     transf_obj = {
         "date_order": dt.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
-        "partner_id": data['order_details']['partner_id'],
-        "partner_ref": data['order_details']['partner_ref'],
+        "partner_id": data["order_details"]["partner_id"],
+        "partner_ref": data["order_details"]["partner_ref"],
         "amount_tax_otros": total_price * PERUVIAN_TAX,
         "order_lines": order_lines,
     }
 
     return transf_obj
 
+
 def order_client_result(order_id):
     result = {
-        "odoo_link": "{}/web#id={}&menu_id=260&cids=1&action=401&model=purchase.order&view_type=form"
-            .format(settings.ODOO_URL, order_id),
-        "odoo_id": order_id
+        "odoo_link": "{}/web#id={}&menu_id=260&cids=1&action=399&model=purchase.order&view_type=form".format(
+            settings.ODOO_URL, order_id
+        ),
+        "odoo_id": order_id,
     }
     return result
