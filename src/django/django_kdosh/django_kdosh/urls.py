bd2e22678f50cd955a8172e5daeb69209e64833b
Roosevelt <19panta97@gmail.com>
2023-02-26 12:42:40 -0500

    add static path

--- src/django/django_kdosh/django_kdosh/urls.py
+++ src/django/django_kdosh/django_kdosh/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,7 +28,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("react.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 admin.site.site_header = "Kdosh Administration"
