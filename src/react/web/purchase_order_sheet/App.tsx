300d21b292d1a2250bc3c2cdd7563fc374e58b87
Freddy Carrillo Tucto <70508243+FreddyCarrillo@users.noreply.github.com>
2023-02-28 15:49:37 -0500

    update

--- src/react/web/purchase_order_sheet/App.tsx
+++ src/react/web/purchase_order_sheet/App.tsx
@@ -11,7 +11,7 @@ import { Typist } from "./Components/Typist/Typist";
 const App = () => {
   const [store, setStore] = useState<Catalog>(stores[0]);
   const [storeTypist, ] = useState<Catalog[]>(storesTypist);
-  const [typist, setTypist] = useState<Catalog>();
+  const [typist, setTypist] = useState<Catalog>({id:0, name:''});
   const [orderGroups, setOrderGroups] = useState<IOrderGrouped[]>([]);
   const [totalQty, setTotalQty] = useState<number>(0);
 
@@ -33,7 +33,7 @@ const App = () => {
   };
 
   const handleStoreUpdateTypist = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const typist = storesTypist.find((store)=> store.id  === Number(e.target.value));
+    const typist = storesTypist?.find((store)=> store.id  === Number(e.target.value))!;
     setTypist(typist);
   };
 
