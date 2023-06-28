2582b002d5cd93ccb5c11841673059a6d193616f
Roosevelt <19panta97@gmail.com>
2023-06-28 18:54:55 -0500

    add pos session admin

--- src/django/django_kdosh/pos_close_control/admin.py
+++ src/django/django_kdosh/pos_close_control/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employee, PosSession
 
 
+@admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "type", "is_used")
 
 
-admin.site.register(Employee, EmployeeAdmin)
+@admin.register(PosSession)
+class PosSessionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "pos_name",
+        "cashier",
+        "manager",
+        "odoo_cash",
+        "odoo_card",
+        "pos_cash",
+        "pos_card",
+        "start_at",
+        "stop_at",
+        "odoo_version",
+    )
+
+    list_filter = ["odoo_version"]
+    search_fields = ["id"]
--- src/django/django_kdosh/pos_close_control/models.py
+++ src/django/django_kdosh/pos_close_control/models.py
@@ -14,6 +14,9 @@ class Employee(models.Model):
     type = models.CharField(max_length=2, choices=TYPE_CHOICES, default=CASHIER)
     is_used = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class PosSession(models.Model):
     id = models.AutoField(primary_key=True)
