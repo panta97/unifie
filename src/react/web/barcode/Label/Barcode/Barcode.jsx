fe4fd44d5ea0e7d5993825e9a62a3f97521d35c9
Marvin Campos Deza <90712919+MarvinH10@users.noreply.github.com>
2024-07-16 14:20:43 -0500

    Update Barcode.jsx

--- src/react/web/barcode/Label/Barcode/Barcode.jsx
+++ src/react/web/barcode/Label/Barcode/Barcode.jsx
@@ -147,7 +147,7 @@ function Barcode({
             <div className="barcode">
               <QRCode
                 value={code}
-                size={50}
+                size={51}
                 level={"H"}
                 renderAs={"svg"}
               ></QRCode>
@@ -158,6 +158,47 @@ function Barcode({
       </div>
     );
   }
+  else if (type === 7) {
+    return (
+      <div className="type-7">
+        <div className="container">
+          <div className="top">
+          <svg width="260" height="130">
+              <path id="curve" d="M 0 120 C 0 120, 130 0, 260 120"></path>
+              <text class="texr">
+              <textPath class="text__path" href="#curve"><h2 className="price">{price}</h2></textPath>
+              </text>
+            
+            </svg>
+            <h2 className="price">{price}</h2>
+
+            <p className="cat">{cats}</p>
+          </div>
+          <div className="middle">
+            <div className="d-attr">
+              <p className="attr">{attr}</p>
+            </div>
+            <div className="barcode">
+              <QRCode
+                value={code}
+                size={100}
+                level={"H"}
+                renderAs={"svg"}
+              ></QRCode>
+            </div>
+            <div className="d-fa-code">
+              <p className="fa-code">{mCode}</p>
+            </div>
+          </div>
+
+          <div className="bottom">
+            <p className="code">{code}</p>
+            <p className="desc">{desc}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Barcode;
+export default Barcode;
