035395827381c03d5c857992e06a1fb5be945334
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2025-03-10 17:39:34 -0500

    Reporte para Raul ok, taxes_id corregido

--- src/django/django_kdosh/product_rpc/reports/connection.py
+++ src/django/django_kdosh/product_rpc/reports/connection.py
@@ -3,15 +3,13 @@ import pandas as pd
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-# Hardcoded credentials
 DB_CREDENTIALS = {
-    "PG_NAME_V11": "kdosh_v11",  # Add the actual value for V11 if needed
-    "PG_NAME_V15": "kdosh_v15",  # Add the actual value for V15 if needed
     "PG_NAME_V17": "kdosh_v17",
     "PG_USER": "postgres",
     "PG_HOST": "77.37.43.105",
     "PG_PWD": "asAS12!@",
 }
+
 
 def get_connstr(odoo_version):
     db_name = ""
@@ -22,9 +20,6 @@ def get_connstr(odoo_version):
     elif odoo_version == 17:
         db_name = DB_CREDENTIALS["PG_NAME_V17"]
 
-    if not db_name or not DB_CREDENTIALS["PG_USER"] or not DB_CREDENTIALS["PG_HOST"] or not DB_CREDENTIALS["PG_PWD"]:
-        raise ValueError("One or more required credentials are not set.")
-
     connstr = "dbname='{}' user='{}' host='{}' password='{}'".format(
         db_name,
         DB_CREDENTIALS["PG_USER"],
@@ -33,8 +28,8 @@ def get_connstr(odoo_version):
     )
     return connstr
 
+
 def select_df(sql, odoo_version):
-    conn = None  # Initialize conn to None
     try:
         connstr = get_connstr(odoo_version)
         conn = psycopg2.connect(connstr)
@@ -43,11 +38,10 @@ def select_df(sql, odoo_version):
     except (Exception, psycopg2.Error) as error:
         print("Failed to get dataframe", error)
     finally:
-        if conn is not None:
-            conn.close()
+        conn.close()
+
 
 def select(sql, odoo_version, params=None):
-    conn = None  # Initialize conn to None
     try:
         connstr = get_connstr(odoo_version)
         conn = psycopg2.connect(connstr)
@@ -61,5 +55,4 @@ def select(sql, odoo_version, params=None):
     except (Exception, psycopg2.Error) as error:
         print("Failed to get dict list", error)
     finally:
-        if conn is not None:
-            conn.close()
+        conn.close()
