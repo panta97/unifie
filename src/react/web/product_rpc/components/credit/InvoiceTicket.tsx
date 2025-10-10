86d9f6ece7734cf1a0307b81d24753e5bdef1f57
Marvin Campos <marvinhectorcamposdeza@gmail.com>
2025-10-10 13:12:32 -0500

    Renderas en qrcode como react qr

--- src/react/web/product_rpc/components/credit/InvoiceTicket.tsx
+++ src/react/web/product_rpc/components/credit/InvoiceTicket.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from "react";
-import * as QRCode from "qrcode.react";
+import QRCode from "react-qr-code";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { selectFormIvoiceStatus } from "../../app/slice/refund/formSlice";
 import {
